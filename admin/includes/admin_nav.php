87cafef329b60af17f5b723f7ecd447648ac2b31
Milan Rajani <milanrajani33@gmail.com>
2023-03-15 12:33:52 +0530

    added features

--- admin/includes/admin_nav.php
+++ admin/includes/admin_nav.php
@@ -96,7 +96,19 @@
                     </ul>
                 </li> -->
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>  
+                
+                <?php
+                if(isset($_SESSION['username'])){
+                    echo $_SESSION['username'];
+                }
+                
+                
+                
+                ?>
+                
+                
+                </a>
                     <ul class="dropdown-menu">
                         <li>
                             <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
